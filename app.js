185c3c571a3344b077ff24f73e38cdff1c9e8bb2
soojeong2 <dltnwud@naver.com>
2024-10-02 16:50:02 +0900

    get url 등 세부사항 수정

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const moment = require("moment");
 const sqlite3 = require("sqlite3");
 const path = require("path");
-
-
+//로그인
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
 
 //database생성
 const db_name = path.join(__dirname, "users.db");
@@ -48,63 +49,151 @@ create table if not exists records (
 
 db.serialize(() => {
   //   db.run(create_sql);
-  db.run(habit_sql);
+  // db.run(habit_sql);
   // db.run(recode_sql);
 });
-
-//로그인세션 마지막에 구현...
 
 const app = express();
 const PORT = 3000;
 
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  expressSession({
+    secret: "sample",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      expires: 60 * 60 * 24,
+    },
+  })
+);
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-//습관목록
-app.get("/habit_list",(req,res) => {
-  // const id = req.params.id;  로그인 구현 후 주석풀기
-  const id = 1;
-  let page = req.query.page ? parseInt(req.query.page) : 1;
-  const limit = 5;
-  const offset = (page - 1) * limit;
+app.use(express.urlencoded({ extended: true })); //form태크의 post요청을 받기위한 설정
 
-  let habit_list_sql = `select h.id, h.habit_name ,h.str_date,h.end_date, (select count(1) from records r where h.id = r.habit_id) as cnt from habits h
-  where h.user_id = ${id}  order by 1 limit ? offset ?;`
+//로그인페이지
+app.get("/login", (req, res) => {
+  console.log(req.query);
+  res.render("login", { message: req.query.message });
+});
 
-  db.all(habit_list_sql, [limit, offset], (err,rows) => {
-    if(err) {
-      res.status(500).send("Server Error");
-    }else{
-      db.get(`select count(1) as count from habits  where user_id = ${id} `, (err,cnt) => {
-        if(err) {
-          res.status(500).send("Error")
-        }else{
-          const totalCnt = cnt.count;
-          const totalPage = Math.ceil(totalCnt / limit);
+//로그인
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const usersSql = `select id, email, password from users where email = '${email}' and password = '${password}'`;
+  let userId = "";
 
-          res.render("habit_list",{
-            habits : rows,
-            currentPage: page,
-            totalPage: totalPage,
-          });
+  db.get(usersSql, (err, row) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Login Error!");
+    } else {
+      if (row) {
+        req.session.user = {
+          id: email,
+          authorized: true,
+          userId: row.id,
+        };
+        res.redirect("/habit_list");
+      } else {
+        res.redirect("/login?message=true");
+      }
+    }
+  });
+});
+
+//회원가입페이지
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+//회원가입
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userSql = "select email from users";
+  const registerSql = `insert into users (name, email, password) values('${name}','${email}','${password}')`;
+  db.all(userSql, (err, rows) => {
+    let isUser = rows.filter((item) => {
+      return item.email == email;
+    });
+
+    if (isUser.length > 0) {
+      res.send(
+        "<script>alert('중복 된 ID입니다.'); window.location.replace('/register')</script>; "
+      );
+    } else {
+      db.run(registerSql, (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send("Server Error!");
+        } else {
+          res.redirect("/login");
         }
       });
     }
   });
 });
 
-app.use(express.urlencoded({ extended: true })); //form태크의 post요청을 받기위한 설정
-
-//습관추가페이지
-app.get("/habit_add",(req,res) => {
-
-  res.render("habit_add")
+//logout
+app.get("/logout", (req, res) => {
+  if (req.session.user) {
+    req.session.user = null;
+  }
+  res.redirect("/login");
 });
 
-//습관 추가 
-app.post("/add_habbit",(req,res) => {
-// const id = req.params.id;  로그인 구현 후 주석풀기
-  const id = 1;
+//습관목록
+app.get("/habit_list", (req, res) => {
+  let page = req.query.page ? parseInt(req.query.page) : 1;
+  const limit = 5;
+  const offset = (page - 1) * limit;
+
+  if (req.session.user) {
+    const id = req.session.user.userId;
+    let habit_list_sql = `select h.id, h.habit_name ,h.str_date,h.end_date, (select count(1) from records r where h.id = r.habit_id) as cnt from habits h
+    where h.user_id = ${id}  order by 1 limit ? offset ?;`;
+    db.all(habit_list_sql, [limit, offset], (err, rows) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Server Error");
+      } else {
+        db.get(
+          `select count(1) as count from habits  where user_id = ${id} `,
+          (err, cnt) => {
+            if (err) {
+              res.status(500).send("Error");
+            } else {
+              const totalCnt = cnt.count;
+              const totalPage = Math.ceil(totalCnt / limit);
+
+              res.render("habit_list", {
+                habits: rows,
+                currentPage: page,
+                totalPage: totalPage,
+              });
+            }
+          }
+        );
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
+});
+
+//습관추가페이지
+app.get("/habit_add", (req, res) => {
+  res.render("habit_add");
+});
+
+//습관 추가
+app.post("/add_habbit", (req, res) => {
+  const id = req.session.user.userId;
+  // const id = 1;
   let sql = `insert into habits (habit_name,str_date,end_date,user_id)
   values ('${req.body.habit_name}','${req.body.str_date}','${req.body.end_date}','${id}')`;
 
@@ -116,11 +205,11 @@ app.post("/add_habbit",(req,res) => {
       res.redirect("/habit_list");
     }
   });
-})
+});
 
 //습관 삭제
-app.get("/delte_habit/:id", (req,res) =>{
-  const id = req.params.id;
+app.get("/delte_habit", (req, res) => {
+  const id = req.query.id;
 
   let sql = `delete from habits where id = ${id}`;
   db.run(sql, (err) => {
@@ -131,53 +220,62 @@ app.get("/delte_habit/:id", (req,res) =>{
       res.redirect("/habit_list");
     }
   });
-})
+});
 
 //습관기록목록
-app.get("/habit_record_list/:id/:habit_name",(req,res) => {
+app.get("/habit_record_list/:id", (req, res) => {
+  const habit_name = req.query.habit_name;
   const id = req.params.id;
+  console.log("1111111111");
+  console.log(id);
   let page = req.query.page ? parseInt(req.query.page) : 1;
   const limit = 5;
   const offset = (page - 1) * limit;
 
-  let habitRecodeSql = `select id, memo,createdAt, habit_id from records r where habit_id = ${id}  order by 1 limit ? offset ?`
-  db.all(habitRecodeSql, [limit, offset],(err,rows) => {
-    if(err) {
-      console.log(err)
-      res.status(500).send("Server Error!")
-    }else{
-      db.get(`select count(1) as count from records where habit_id = ${id}`, (err,cnt) => {
-        if(err) {
-          res.status(500).send("Error")
-        }else{
-              const totalCnt = cnt.count;
-              const totalPage = Math.ceil(totalCnt / limit);
+  let habitRecodeSql = `select id, memo,createdAt, habit_id from records r where habit_id = '${id}'  order by 1 limit ? offset ?`;
+  db.all(habitRecodeSql, [limit, offset], (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Server Error!");
+    } else {
+      db.get(
+        `select count(1) as count from records where habit_id = ${id}`,
+        (err, cnt) => {
+          if (err) {
+            res.status(500).send("Error");
+          } else {
+            const totalCnt = cnt.count;
+            const totalPage = Math.ceil(totalCnt / limit);
 
-              res.render("habit_record_list", {
-                habitRecode : rows,
-                name : req.params.habit_name,
-                habit_id : id,
-                currentPage: page,
-                totalPage: totalPage,
-              })
+            res.render("habit_record_list", {
+              habitRecode: rows,
+              habit_name: habit_name,
+              habit_id: id,
+              currentPage: page,
+              totalPage: totalPage,
+            });
           }
-      });
+        }
+      );
     }
-  })
-
-  
-})
+  });
+});
 
 //습관기록추가페이지
-app.get("/habit_record_add/:id/:name",(req,res) => {
-  res.render("habit_record_add",{
-    habit_id : req.params.id,
-    habit_name : req.params.name
-  })
-})
+app.get("/habit_record_add/:id", (req, res) => {
+  console.log("22222222222");
+
+  console.log(req.params);
+  console.log(req.query);
+  res.render("habit_record_add", {
+    habit_id: req.params.id,
+    habit_name: req.query.habit_name,
+  });
+});
 
 //습관기록 추가
-app.post("/add_habit_record",(req,res) =>{
+app.post("/add_habit_record", (req, res) => {
+  console.log("3333333333");
   console.log(req.body);
   let sql = `insert into records (memo,habit_id) 
   values ('${req.body.memo}','${req.body.habit_id}')`;
@@ -187,13 +285,13 @@ app.post("/add_habit_record",(req,res) =>{
       console.log(err);
       res.status(500).send("Server Error");
     } else {
-      res.redirect(`/habit_record_list/${req.body.habit_id}/${req.body.habit_name}`);
+      res.redirect(`/habit_record_list/${req.body.habit_id}`);
     }
   });
 });
 
 //습관기록 삭제
-app.get("/delte_habit_record/:id/:habit_id/:name", (req,res) =>{
+app.get("/delte_habit_record/:id", (req, res) => {
   const id = req.params.id;
 
   let sql = `delete from records where id = ${id}`;
@@ -202,11 +300,11 @@ app.get("/delte_habit_record/:id/:habit_id/:name", (req,res) =>{
       console.log(err);
       res.status(500).send("Server Error");
     } else {
-      res.redirect(`/habit_record_list/${req.params.habit_id}/${req.params.name}`);
+      res.redirect(`/habit_record_list/${req.query.id}?name=${req.query.name}`);
     }
   });
-})
- 
+});
+
 app.listen(PORT, () => {
   console.log("listening on listServer");
 });
